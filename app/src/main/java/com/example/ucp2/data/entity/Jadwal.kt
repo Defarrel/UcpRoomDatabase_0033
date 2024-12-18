c32ec3ab646d898bd0e57accbd90e132600947b2
defarrel <Defarrel>
2024-12-18 17:55:56 +0700

    Mengubah id disetiap entity menjadi autogenerate (yg tdi salah commit malah commit jadwalDao)

--- app/src/main/java/com/example/ucp2/data/entity/Jadwal.kt
+++ app/src/main/java/com/example/ucp2/data/entity/Jadwal.kt
@@ -5,8 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "jadwal")
 data class Jadwal(
-    @PrimaryKey
-    val id: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val namaDokter: String,
     val namaPasien: String,
     val noHp: String,
