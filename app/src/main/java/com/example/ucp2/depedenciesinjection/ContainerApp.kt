cff06e6ad3410a9b8bbdbe3f1dd16da74a255872
defarrel <Defarrel>
2024-12-18 17:53:15 +0700

    Menambahkan InterfaceContainerApp dan isi ConteinerApp

--- app/src/main/java/com/example/ucp2/depedenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2/depedenciesinjection/ContainerApp.kt
@@ -1,5 +1,23 @@
 package com.example.ucp2.depedenciesinjection
 
-interface ContainerApp {
+import android.content.Context
+import com.example.ucp2.data.database.HealtDatabase
+import com.example.ucp2.repository.LocalRepositoryDokter
+import com.example.ucp2.repository.LocalRepositoryJadwal
+import com.example.ucp2.repository.RepositoryDokter
+import com.example.ucp2.repository.RepositoryJadwal
 
+interface InterfaceContainerApp {
+    val repositoryDokter: RepositoryDokter
+    val repositoryJadwal: RepositoryJadwal
+}
+
+class ContainerApp(private val context: Context) : InterfaceContainerApp {
+    override val repositoryDokter: RepositoryDokter by lazy{
+        LocalRepositoryDokter(HealtDatabase.getDatabase(context).dokterDao())
+    }
+
+    override val repositoryJadwal: RepositoryJadwal by lazy{
+        LocalRepositoryJadwal(HealtDatabase.getDatabase(context).jadwalDao())
+    }
 }
