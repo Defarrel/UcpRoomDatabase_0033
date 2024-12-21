7c6226f175345e9118f3cac5723813330129f5ce
defarrel <Defarrel>
2024-12-21 17:09:32 +0700

    Mengubah tampilan dropdown

--- app/src/main/java/com/example/ucp2/ui/customwidget/DynamicSelectField.kt
+++ app/src/main/java/com/example/ucp2/ui/customwidget/DynamicSelectField.kt
@@ -1,6 +1,8 @@
 package com.example.ucp2.ui.customwidget
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -14,7 +16,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.example.ucp2.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -38,13 +45,23 @@ fun DynamicSelectField(
             readOnly = true,
             value = selectedValue,
             onValueChange = {},
-            label = { Text(text = label) },
+            label = { Text(text = label, modifier = Modifier, color = colorResource(id = R.color.black)) },
             placeholder = { Text(text = placeholder)},
             trailingIcon = {
                 ExposedDropdownMenuDefaults.TrailingIcon(
                     expanded = expanded
                 ) },
-            colors = OutlinedTextFieldDefaults.colors(),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = colorResource(id = R.color.black),
+                focusedTextColor = colorResource(id = R.color.black),
+                unfocusedTextColor = colorResource(id = R.color.black),
+                cursorColor = colorResource(id = R.color.black)),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            singleLine = true,
+            shape = RoundedCornerShape((50.dp)),
             modifier = Modifier
                 .menuAnchor()
                 .fillMaxWidth()
